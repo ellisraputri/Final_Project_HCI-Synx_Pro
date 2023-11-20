d6a9fb50c0f147565794c9c50a09ab25e0c3d98d
ellisraputri <ellisraputri@gmail.com>
2023-11-20 23:28:35 +0700

    tips and trick (c)

---
+++ tips.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    let value=1;
+    $(".page2").hide();
+    $(".page3").hide();
+    $(".page4").hide();
+    $(".page5").hide();
+    $('.buttonprev').attr('disabled','disabled');
+
+    $.fn.checkvalue = function(value) { 
+        if(value==1){
+            $('.buttonprev').attr('disabled','disabled');
+        }
+        else{
+            $('.buttonprev').removeAttr('disabled');
+        }
+    
+        if(value==5){
+            $('.buttonnext').attr('disabled','disabled');
+            $(".buttonnext").off("mouseenter mouseleave");
+        }
+        else{
+            $('.buttonnext').removeAttr('disabled');
+        }
+    };
+
+    $(function(){
+        $(".flip").flip({
+            trigger: 'click',
+            axis:'x'
+        });
+    });
+
+    $(".buttonnext").click(function(){
+        value+=1;
+        $('.page'+(value-1)).hide("slide", { direction: "left", distance:"2%"}, 300);
+        $('.page'+(value)).delay(300.001).show("slide", { direction: "right", distance:"2%"}, 300);
+        $('.mid').text(value + " / 5")
+        $.fn.checkvalue(value);
+    });
+
+    $(".buttonprev").click(function(){
+        value-=1;
+        $('.page'+(value+1)).hide("slide", { direction: "left", distance:"2%"}, 300);
+        $('.page'+(value)).delay(300.001).show("slide", { direction: "right", distance:"2%"}, 300);
+        $('.mid').text(value + " / 5")
+        $.fn.checkvalue(value);
+    });
+
+});
+
