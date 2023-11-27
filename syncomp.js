736d4ce9e6857f85efcf2147fca33120b3193cd3
ellisraputri <ellisraputri@gmail.com>
2023-11-28 00:03:52 +0700

    syncomp progress 1

--- syncomp.js
+++ syncomp.js
@@ -7,20 +7,47 @@ $(document).ready(function(){
             $(this).children('ul').slideDown('fast');
         },
         function () {
-            $('ul', this).slideUp('fast');            
+            $('ul', this).slideUp();            
     });
 
-    $('.lala').hover(function(){
+    $('.lala1').hover(function(){
         $(this).addClass('activ');
-        $('.downsym').fadeOut('fast');
-        $('.upsym').delay(400).fadeIn();
-        
+        $('.lala1 .downsym').hide();
+        $('.lala1 .upsym').delay(400).show();
+    });
+
+    $('.lala2').hover(function(){
+        $(this).addClass('activ');
+        $('.lala2 .downsym').hide();
+        $('.lala2 .upsym').delay(400).show();
+        $('.menulala2').slideDown();
+    });
+
+    $('.lala3').hover(function(){
+        $(this).addClass('activ');
+        $('.lala3 .downsym').hide();
+        $('.lala3 .upsym').delay(400).show();
+        $('.menulala3').slideDown();
+    });
+
+    $('.columnlala2').mouseleave(function(){
+        $('.lala2').removeClass('activ');
+        $('.lala2 .upsym').hide();
+        $('.lala2 .downsym').delay(400).show();
+        $('.menulala2').slideUp();
+    });
+
+    $('.columnlala3').mouseleave(function(){
+        $('.lala3').removeClass('activ');
+        $('.lala3 .upsym').hide();
+        $('.lala3 .downsym').delay(400).show();
+        $('.menulala3').slideUp();
     });
 
     $('.centerin3').mouseleave(function(){
-        $('.lala').removeClass('activ');
-        $('.upsym').fadeOut('fast');
-        $('.downsym').delay(450).fadeIn();
+        $('.lala1').removeClass('activ');
+        $('.lala1 .upsym').hide();
+        $('.lala1 .downsym').delay(400).show();
     });
     
 });
