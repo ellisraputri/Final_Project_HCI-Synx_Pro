043af76b25d50964a46d751b9c78b61d94e0cc55
ellisraputri <ellisraputri@gmail.com>
2023-11-29 14:21:43 +0700

    syncomp progress 3

--- syncomp.js
+++ syncomp.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.downsym').show();
     $('.upsym').hide();
+    $('.resultss').hide();
+
     $("#nav li").hover(
         function(){
             $(this).children('ul').hide();
@@ -49,5 +51,58 @@ $(document).ready(function(){
         $('.lala1 .upsym').hide();
         $('.lala1 .downsym').delay(400).show();
     });
+
+    let click_compare_value, click_language1_value, click_language2_value;
+
+    $('.menus1').click(function(){
+        click_compare_value=$(this).attr('value');
+        $('.comparemenu').html(click_compare_value + '&ensp;\
+        <i class="fa-solid fa-caret-down downsym"></i>\
+        <i class="fa-solid fa-caret-up upsym"></i>');
+        
+        $('.menulala1').slideUp(1);
+    });
+
+    $('.menus2').click(function(){
+        click_language1_value = $(this).attr('value');
+        $('.languagemenu1').html(click_language1_value + '&ensp;\
+        <i class="fa-solid fa-caret-down downsym"></i>\
+        <i class="fa-solid fa-caret-up upsym"></i>');
+        
+        $('.menulala2').slideUp(1);
+    });
+
+    $('.menus3').click(function(){
+        click_language2_value = $(this).attr('value');
+        $('.languagemenu2').html(click_language2_value + '&ensp;\
+        <i class="fa-solid fa-caret-down downsym"></i>\
+        <i class="fa-solid fa-caret-up upsym"></i>');
+        
+        $('.menulala3').slideUp(1);
+    });
+    
+
+    $('button').click(function(){
+        if (click_compare_value==undefined || click_language1_value==undefined || click_language2_value==undefined){
+            let msg = 'PLEASE FILL IN:';
+            if(click_compare_value==undefined){
+                msg = msg + '\n- "What to compare?" section';
+            }
+            if(click_language1_value==undefined){
+                msg = msg + '\n- "Language 1" section';
+            }
+            if(click_language2_value==undefined){
+                msg = msg + '\n- "Language 2" section';
+            }
+            alert(msg)
+        }
+        
+
+    });
+
+    
+        
+    
+    
     
 });
